25736b241f3b5e55669d1483b33a41d420c28a10
moeinnajafi1998 <moeinnajafi1996@gmail.com>
2024-10-05 17:51:50 +0330

    first example=> hello world!

---
+++ 1.py
@@ -0,0 +1,15 @@
+# What is Python?
+# Python is a high-level, interpreted programming language known for its readability and simplicity. 
+# It was created by Guido van Rossum and first released in 1991. Python's design philosophy emphasizes code readability, 
+# and its syntax allows programmers to express concepts in fewer lines of code than possible in languages such as C++ or Java.
+
+# Key Features of Python
+# 1.	Easy to Learn and Use: Python has a simple and straightforward syntax that makes it easy for beginners to learn and use.
+# 2.	Interpreted Language: Python is executed line by line, which makes debugging easier.
+# 3.	Cross-platform: Python can run on various operating systems like Windows, macOS, and Linux.
+# 4.	Open Source: Python is free to use and distribute, including for commercial purposes.
+# 5.	Extensive Standard Library: Python comes with a vast standard library that includes modules and packages for various tasks.
+# 6.	Versatile: Python is used for web development, data analysis, artificial intelligence, scientific computing, and more.
+# First script:
+
+print("Hello, World!")
